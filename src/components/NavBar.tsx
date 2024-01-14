77c3a1302960d6c4d24da08168d36325f21bd70c
pusit-hanp <pusit.hanp@gmail.com>
2024-01-14 14:11:18 -0700

    Revert "Update Logo"
    This reverts commit bd10c2e9e34bdd2e08fc2f01133412efdec52877.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
@@ -9,7 +10,7 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
-      <Text fontSize="3xl">Logo</Text>
+      <Image src={logo} boxSize="60px" />
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
