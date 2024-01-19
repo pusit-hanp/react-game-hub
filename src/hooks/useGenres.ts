3b57a7ffaa9cae0f887c2351ccf9375e6c9042a8
pusit-hanp <pusit.hanp@gmail.com>
2024-01-18 22:50:31 -0700

    Refactor - fetch platforms with React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -15,7 +15,7 @@ const useGenres = () =>
     queryFn: () =>
       apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
     staleTime: 24 * 60 * 60 * 1000, // 24h
-    initialData: { count: genres.length, results: genres },
+    initialData: genres,
   });
 
 export default useGenres;
