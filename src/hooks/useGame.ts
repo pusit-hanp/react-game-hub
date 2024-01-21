672c2fd912abd971f21a72c003713fcbfaeccea1
pusit-hanp <pusit.hanp@gmail.com>
2024-01-20 22:51:36 -0700

    Fetch game details

---
+++ src/hooks/useGame.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
+import APIClitent from "../services/api-client";
+import { Game } from "./useGames";
+
+const apiClient = new APIClitent<Game>("/games");
+
+const useGame = (slug: string) => {
+  return useQuery({
+    queryKey: ["game", slug],
+    queryFn: () => apiClient.get(slug),
+    staleTime: ms("24h"),
+  });
+};
+
+export default useGame;
